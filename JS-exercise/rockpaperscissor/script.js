da7c5c351942a366e6ab770aaaaf2b1f97c80605
santi <santi.cacciola@hotmail.it>
2022-01-09 14:15:30 +0000

    completed RPS-UI

--- JS-exercise/rockpaperscissor/script.js
+++ JS-exercise/rockpaperscissor/script.js
@@ -1,36 +1,36 @@
+let pSelection = undefined;
+
+const rock = document.getElementById("rock").addEventListener("click", () => {
+    pSelection = "Rock";
+    game();
+});
+const paper = document.getElementById("paper").addEventListener("click", () => {
+    pSelection = "Paper";
+    game();
+});
+const scissor = document.getElementById("scissor").addEventListener("click", () => {
+    pSelection = "Scissor";
+    game();
+});
+
 function game() {
-    /*main point of entry for the game.loops 5 times the game. stores the values of the player slection and computer selection in 2 variables and then compares them in the checkwinner function.*/
+    document.getElementById("result").textContent = "";
     console.log("GAME START");
-    for (let i = 0; i < 5; i++) {
-        var pSelection = playerPlay();
-        var cSelection = computerPlay();
-        checkWinner(pSelection, cSelection);
-    }
+    let cSelection = computerPlay();
+    checkWinner(pSelection, cSelection);
     console.log("GAME OVER");
 }
 
 function checkWinner(pSelection, cSelection) {
     /*manually compares all states and outputs a message*/
-    if (pSelection == cSelection) console.log("TIE");
-    if (pSelection == "Paper" && cSelection == "Rock") console.log("WIN: Paper beats Rock");
-    if (pSelection == "Rock" && cSelection == "Scissor") console.log("WIN: Rock beats Scissor");
-    if (pSelection == "Scissor" && cSelection == "Paper") console.log("WIN: Scissor beats paper");
-    if (cSelection == "Paper" && pSelection == "Rock") console.log("LOSE: Paper beats Rock");
-    if (cSelection == "Rock" && pSelection == "Scissor") console.log("LOSE: Rock beats Scissor");
-    if (cSelection == "Scissor" && pSelection == "Paper") console.log("LOSE: Scissor beats paper");
-}
-
-function playerPlay() {
-    /** loops until the player writes the right word, no matter the capitalization as it will modify the inputs. return the player's input*/
-    while (true) {
-        let selection = prompt("Select Paper/Rock/Scissor");
-        selection = selection[0].toUpperCase() + selection.substring(1).toLowerCase();
-        if (selection == "Rock" || selection == "Paper" || selection == "Scissor") {
-            console.log(selection);
-            return selection;
-        }
-    }
-
+    let result = document.getElementById("result");
+    if (pSelection == cSelection) result.textContent = "TIE";
+    if (pSelection == "Paper" && cSelection == "Rock") result.textContent = "WIN: Paper beats Rock";
+    if (pSelection == "Rock" && cSelection == "Scissor") result.textContent = "WIN: Rock beats Scissor";
+    if (pSelection == "Scissor" && cSelection == "Paper") result.textContent = "WIN: Scissor beats Paper";
+    if (cSelection == "Paper" && pSelection == "Rock") result.textContent = "LOSE: Paper beats Rock";
+    if (cSelection == "Rock" && pSelection == "Scissor") result.textContent = "LOSE: Rock beats Scissor";
+    if (cSelection == "Scissor" && pSelection == "Paper") result.textContent = "LOSE: Scissor beats Paper";
 }
 
 function computerPlay() {
