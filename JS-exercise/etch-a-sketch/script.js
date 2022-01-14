8bb522013f9cdf6e93f94382f15f63a312b22ea6
santi <santi.cacciola@hotmail.it>
2022-01-14 11:22:26 +0000

    added randomcolors and eraser, now the logic can be changed easily as it select over a switch case

--- JS-exercise/etch-a-sketch/script.js
+++ JS-exercise/etch-a-sketch/script.js
@@ -1,31 +1,20 @@
-// function createGrid(x) {
-//     let divs = "";
-//     for (let i = 0; i < x; i++) {
-//         divs += '<div class = "row">'
-//         for (let j = 0; j < x; j++) {
-//             divs += `<div class = "cell"></div>`
-//         }
-//         divs += "</div>"
-//     }
-//     container.innerHTML = divs;
-//     //WORKS: but manipulates the string and uses innerHTML. needs to be implemented with createElement and appendChild as it would be a better exercise
-// }
-
-//createGrid(16);
-
 const container = document.getElementById("container");
 const btnBlack = document.getElementById("black");
 const btnClear = document.getElementById("clear");
 const btnRandom = document.getElementById("random");
-btnClear.addEventListener("click", clear);
-let defautColor = "red";
+const btnEraser = document.getElementById("eraser");
+
+let brushColor = "black"; /**default color */
 let defaultSize = 16;
 
-function createGrid(defaultSize) {
-    /** let div = document.createElement("div");
-    container.appendChild(div).className = "cell";*/
-    /**TEST; create one div element TESTS*/
+btnClear.addEventListener("click", clear);
+btnBlack.addEventListener("click", () => { brushColor = "black" });
+btnRandom.addEventListener("click", () => { brushColor = "rainbow" });
+btnEraser.addEventListener("click", () => { brushColor = "eraser" });
+/**EXPLANATION: each button change the string value of brushColor, this will be used in the colors() method in a switch case to select the color */
 
+function createGrid(defaultSize) {
+    /**TODO: change the size of the grid without changing the size of the page, by reducing the size of the cells */
     for (let j = 0; j < defaultSize; j++) {
         let row = document.createElement("div");
         row.classList.add("row"); //adds css class "row"
@@ -39,16 +28,14 @@ function createGrid(defaultSize) {
     /**EXPLANATION: the first loop initialize a row by creating a div element, the second loop fills the row with cells/div which it appends to the row (CSS styling of display:inline-block on row puts them on a single horizontal line), final line which is still in the scope of the first loop append the newly created row to the container element */
 }
 
-function hover(color) {/**CHANGE TO: remove "color" argument. leave blank*/
+function hover() {
     let divs = document.querySelectorAll(".cell");
     divs.forEach(div => {
         div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = color;// CHANGE TO: div.style.backgroundColor = defaultColor;
-            /**maybe it needs to point to the global variable instead? and no argument passed, otherwise it attach 
-            the event listener with the color we decide.*/
+            div.style.backgroundColor = colors();
         });
     });
-    /**EXPLANATION: i need to attach an evnt listener that chenge the color of the div when the mouse is on it. i select all the queries of the class "cell", i cycle via a froEach loop and i add the vent listener that change the color */
+    /**EXPLANATION: i attach an event listener that chenge the color of the div when the mouse is on it. i select all the queries of the class "cell", i cycle via a forEach loop and i add the event listener that change the color, which is selected by the method colors()  */
 }
 
 function clear() {
@@ -56,8 +43,36 @@ function clear() {
     divs.forEach(div => {
         div.style.backgroundColor = "white";
     });
-
     /**EXPLANATION:cycle to all the divs and change the backgroudnColor style to "white"*/
 }
+
+function colors() {
+    switch (brushColor) {
+        case "rainbow":
+            const randomR = Math.floor(Math.random() * 256);
+            const randomG = Math.floor(Math.random() * 256);
+            const randomB = Math.floor(Math.random() * 256);
+            return `rgb(${randomR}, ${randomG}, ${randomB})`;
+        case "black":
+            return "black";
+        case "eraser":
+            return "white";
+    }
+    /**EXPLANATION: each button change the value of the variable "brushColor" into 3 possible state(raibow, black, eraser). the method in a switch case checks which value is stored in brushColor and return the value for the current usage */
+}
+
+// function createGrid(x) {
+//     let divs = "";
+//     for (let i = 0; i < x; i++) {
+//         divs += '<div class = "row">'
+//         for (let j = 0; j < x; j++) {
+//             divs += `<div class = "cell"></div>`
+//         }
+//         divs += "</div>"
+//     }
+//     container.innerHTML = divs;
+//     //WORKS: but manipulates the string and uses innerHTML. needs to be implemented with createElement and appendChild as it would be a better exercise
+// }
+
 createGrid(defaultSize);
-hover(defautColor);
+hover();
