9248291d7d71c67b82d0035ce86315c49ec42f04
santi <santi.cacciola@hotmail.it>
2022-02-27 11:30:22 +0000

    added console.log for clarification on data fetch

--- JS-exercise/Weather_App/src/APIFunctions.js
+++ JS-exercise/Weather_App/src/APIFunctions.js
@@ -8,7 +8,7 @@ async function geolocation() {
     let lon = promise.coords.longitude
     const data = await fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=f01e320c417dd9583e7ed5e57fb13e71`, { mode: "cors" })
     const geoData = await data.json()
-    console.log(geoData, "geolocation")
+    console.log(geoData, "user geolocation data fetch")
     DomFunctions.search(geoData[0].name)
 }
 
@@ -19,7 +19,7 @@ async function getCoords(url) {
 
     const response1 = await fetch(url, { mode: "cors" });
     const geoData = await response1.json();
-    console.log(geoData, "geodata") ///////////////
+    console.log(geoData, "openweathermap reverse geolocation API fetch")
 
     const name = geoData[0].name;
     const country = geoData[0].country;
@@ -28,7 +28,7 @@ async function getCoords(url) {
 
     const response2 = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=standard&exclude=minutely&appid=f01e320c417dd9583e7ed5e57fb13e71`, { mode: "cors" })
     const weatherData = await response2.json();
-    console.log(weatherData, "weatherData") /////////////////////
+    console.log(weatherData, "openweathermap onecall API fetch")
 
     return [name, country, weatherData];
 }
